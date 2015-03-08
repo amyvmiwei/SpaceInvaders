1e12859cb178e6cf3763cb3af00a19076fdc09c4
laserbeam897 <joshuajames355@gmail.com>
2015-03-08 17:41:26 +0000

    Added blank UE4 project.

---
+++ Source/SpaceInvaders/SpaceInvadersGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SpaceInvadersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEINVADERS_API ASpaceInvadersGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
